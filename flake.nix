d4678528f68f637010b5f366f7f84b550375d92b
trev <me@trev.xyz>
2025-07-21 05:05:31 -0400

    bump: v0.0.6 -> v0.0.7

--- flake.nix
+++ flake.nix
@@ -74,7 +74,7 @@
       }:
         pkgs.buildNpmPackage (finalAttrs: {
           pname = "trevbar";
-          version = "0.0.6";
+          version = "0.0.7";
           src = ./.;
           nodejs = pkgs.nodejs_22;
 
