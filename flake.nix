d253d8c3ecab141b6d618721e67629f9c026829d
doctor <doctor@trev.xyz>
2026-06-18 22:03:24 +0000

    bump: v0.5.3 -> v0.6.0

--- flake.nix
+++ flake.nix
@@ -197,7 +197,7 @@
           with pkgs.lib;
           pkgs.buildNpmPackage (final: {
             pname = "trevbar";
-            version = "0.5.3";
+            version = "0.6.0";
             nodejs = pkgs.nodejs_24;
 
             src = fileset.toSource {
